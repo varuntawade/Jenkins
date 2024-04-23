292d0b82eaf490e6b83b04aacf6489c7b85b08e4
varuntawade <166566952+varuntawade@users.noreply.github.com>
2024-04-24 01:24:53 +0530

    Fix Typos

--- Configuration_as_Code/install_plugins.sh
+++ Configuration_as_Code/install_plugins.sh
@@ -3,9 +3,9 @@
 #!/bin/bash
 set -x
 for plugin in $(cat plugins.txt); do \
-    java -jar /home/varun/Jekins-cli/jenkins-cli.jar \
-    -s http://localhost:8080/ \
+    java -jar /home/varun/Jenkins-cli/jenkins-cli.jar \
+    -s http://laptop-vanv0g5o:8080/ \
     -auth ${{ secrets.JENKINS_USER }}:${{ secrets.JENKINS_PASSWORD }} \
     install-plugin $plugin; \
 done
-java -jar /home/varun/Jekins-cli/jenkins-cli.jar -s http://laptop-vanv0g5o/:8080/ -auth ${{ secrets.JENKINS_USER }}:${{ secrets.JENKINS_PASSWORD }} safe-restart
+java -jar /home/varun/Jenkins-cli/jenkins-cli.jar -s http://laptop-vanv0g5o/:8080/ -auth ${{ secrets.JENKINS_USER }}:${{ secrets.JENKINS_PASSWORD }} safe-restart
